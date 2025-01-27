a4a6935bdc91dff25cbce6673ca6ffa146344999
edemarest <demarest.e@northeastern.edu>
2025-01-27 12:35:00 -0500

    Added button on Kambaz to go to landing page

--- src/Kambaz/index.tsx
+++ src/Kambaz/index.tsx
@@ -1,17 +1,29 @@
-import { Routes, Route, Navigate }
-  from "react-router";
+import { Routes, Route, Navigate, useNavigate } from "react-router";
 import Account from "./Account";
 import Dashboard from "./Dashboard";
 import KambazNavigation from "./AccNavigation";
 import Courses from "./Courses";
 import "./styles.css";
+
 export default function Kambaz() {
+
+  const goToLabs = () => {
+    window.location.href = "/#/Labs"; 
+  };
+
   return (
     <div id="wd-kambaz">
+      <div className="wd-top-right-button">
+        <button onClick={goToLabs} className="wd-landing-button">
+          Landing Page
+        </button>
+      </div>
       <table width="100%">
-        <tr><td valign="top">
-          <KambazNavigation />
-        </td><td valign="top">
+        <tr>
+          <td valign="top">
+            <KambazNavigation />
+          </td>
+          <td valign="top">
             <div className="wd-main-content-offset p-3">
               <Routes>
                 <Route path="/" element={<Navigate to="Account" />} />
@@ -20,9 +32,9 @@ export default function Kambaz() {
                 <Route path="/Courses/:cid/*" element={<Courses />} />
               </Routes>
             </div>
-          </td></tr>
+          </td>
+        </tr>
       </table>
     </div>
-
   );
 }
