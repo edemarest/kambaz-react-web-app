50422bf71eecfdfba3b085e80c3047f8c5e1ab2e
edemarest <demarest.e@northeastern.edu>
2025-02-11 21:35:21 -0500

    Landing page button

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -3,9 +3,21 @@ import { Route, Routes, Navigate } from "react-router";
 import TOC from "./TOC";
 import Lab2 from "./Lab2"; 
 import Lab3 from "./Lab3";
+
 export default function Labs() {
+
+  const goToLandingPage = () => {
+    window.location.href = "/#/Kambaz"; 
+  };
+
   return (
     <div>
+      <div className="wd-top-right-button">
+        <button onClick={goToLandingPage} className="wd-landing-button">
+          Landing Page
+        </button>
+      </div>
+
       <p><strong>Assignment by:</strong> Ella Demarest</p>
       <p><strong>Section:</strong> CS4550.37031.202530</p>
       <h1>Labs</h1>
